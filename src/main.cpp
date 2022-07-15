f1dddd42ae00d9b5f183365ca6ea8153fcc5f104
Tony Pieri <ajpieri@gmail.com>
2022-07-15 00:29:00 -0500

    updating print statement

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "You have entered " << argc
+    cout << "You have entered: " << argc
          << " arguments:" << "\n";
   
     for (int i = 0; i < argc; ++i)
